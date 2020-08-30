cddc3ed0198b7409c2eb1b52e657c076e1ceb263
Eugene Koontz <ekoontz@hiro-tan.org>
2020-08-30 17:59:45 +0200

    diff minimization

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :source-paths ["src/main/clojure" "src/main/clojurescript"]
   :test-paths ["src/test/clojure" "src/test/clojurescript"]
   :resource-paths ["src/main/resources" "src/test/resources" "target/gen-resources"]
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.0-beta8"]
                  [org.clojure/clojurescript "1.10.439"]
                  [com.cemerick/piggieback "0.2.2"]
                  [org.clojure/tools.nrepl "0.2.13"]
