b2175dae56cc5695e9a1b5a75fec4fb8e7378f16
Eugene Koontz <ekoontz@hiro-tan.org>
2020-09-05 12:23:54 +0200

    upgrade dependencies; all tests pass EXCEPT clojurescript ones; something about nashorn

--- src/test/clojurescript/clojure/data/xml/cljs_repls.clj
+++ src/test/clojurescript/clojure/data/xml/cljs_repls.clj
@@ -1,16 +1,16 @@
 (ns clojure.data.xml.cljs-repls
   (:require
    [cljs.repl :as repl]
-   [cljs.repl.nashorn :as repl-nh]
-   [cemerick.piggieback :as pback]
+;;   [cljs.repl.nashorn :as repl-nh]
+   [cider.piggieback :as pback]
    [cljs.closure :as closure]
    [figwheel-sidecar.repl-api :refer [start-figwheel! stop-figwheel! cljs-repl]]))
 
 (defn nashorn-env []
-  (let [{:as env :keys [engine]} (repl-nh/repl-env)]
-    (repl-nh/eval-resource engine "dxml-nashorn.generated.js" true)
-    env))
-
+;;  (let [{:as env :keys [engine]} (repl-nh/repl-env)]
+;;    (repl-nh/eval-resource engine "dxml-nashorn.generated.js" true)
+;;    env))
+)
 (def handle-redirect (constantly {:status 307 :headers {"Location" "/cljs-tests/index.html"}}))
 
 (defn repl-figwheel! []
